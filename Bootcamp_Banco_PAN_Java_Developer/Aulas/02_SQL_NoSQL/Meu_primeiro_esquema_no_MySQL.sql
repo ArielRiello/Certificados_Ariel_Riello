cf59128de39d1028bc02a374a8addc6effb81ad3
ArielRiello <riello.programmer@gmail.com>
2024-03-05 11:31:42 -0300

    Bootcamp_Banco_PAN_Java_Developer

---
+++ Bootcamp_Banco_PAN_Java_Developer/Aulas/02_SQL_NoSQL/Meu_primeiro_esquema_no_MySQL.sql
@@ -0,0 +1,17 @@
+show databases;
+create database if not exists fist_example;
+use first_exemple;
+CREATE TABLE person(
+	person_id smallint unsigned,
+	fname varchar(20),
+	lname varchar(20),
+	gender enum('M','F'),
+	birth_date DATE,
+	street varchar(30),
+	city varchar(20),
+	state varchar(20),
+	country varchar(20),
+	postal_code varchar(20),
+    constraint pk_person primary key (person_id)
+);
+desc person;
